3cb15cb8522544a1939310265f042586a2a85b4f
Mr. Goodhand <Zaeo1137@gmail.com>
2022-10-23 12:53:19 -0400

    Uploaded my First Python Program
    The Python program gets input from the user, counts the characters of their input, and asks a question to a user. Then, it snarkily ends.

---
+++ InputAndPrintStory.py
@@ -0,0 +1,8 @@
+character_name = input("Tell me your name...")
+character_name_length = str(len(character_name))
+
+
+print("Hello, " + character_name + ".")
+print("There are " + character_name_length + " characters in your name.")
+answer_1 = input("Does that surprise you that I know this?")
+print(answer_1 +", is it? How...Quaint. Begone with you, I have more important matters to attend to.")
